c43bf78c9872b6e74fbf783adfb6914e192c8a97
ljurca <lmjurca@dons.usfca.edu>
2024-10-30 10:42:14 -0700

    updating mongo in server.js

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ const { OpenAI } = require('openai');
 const mongoose = require('mongoose');
 const axios = require('axios'); // Import axios here
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB successfully');
+  })
+  .catch(err => {
+    console.error('MongoDB connection error:', err);
+  });
 
 // Import models
 const Interaction = require('./models/Interaction');
